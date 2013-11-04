1a953bf45856820d5eebf8ab6373a8c8efc50a00
Stefan Prodan <stefan.prodan@gmail.com>
2013-11-04 15:37:02 +0200

    resources and custom error pages

---
+++ WebApplication/Controllers/ErrorController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Error");
+        }
+
+        public ViewResult NotFound()
+        {
+            return View("NotFound");
+        }
+	}
+}
